bd13deea03804cf92604c7c28348fa7566845134
gabriel140282 <gkaakmor@myuax.com>
2025-03-04 09:50:17 +0100

    V0.4

---
+++ src/main/java/org/example/Terrestre.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Terrestre extends Habitat {
+    public Terrestre(String nombre, double temperatura, double humedad, String limpieza) {
+        super(nombre, temperatura, humedad, limpieza);
+    }
+
+    @Override
+    public void monitor() {
+        System.out.println("\nMonitoreando hábitat terrestre: " + getNombre());
+        System.out.println("Temperatura: " + getTemperatura() + "°C, Humedad: " + getHumedad() + "%, Limpieza: " + getLimpieza());
+    }
+}
+
