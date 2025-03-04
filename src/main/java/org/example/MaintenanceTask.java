f26f39e10a06c106fb4e2978812fb5a54625bf81
gabriel140282 <gkaakmor@myuax.com>
2025-03-04 12:51:02 +0100

    V1.1

---
+++ src/main/java/org/example/MaintenanceTask.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class MaintenanceTask {
+    private String descripcion;
+    private LocalDate fechaProgramada;
+    private String estado;
+
+    public MaintenanceTask(String descripcion, LocalDate fechaProgramada) {
+        this.descripcion = descripcion;
+        this.fechaProgramada = fechaProgramada;
+        this.estado = "Pendiente";
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public LocalDate getFechaProgramada() {
+        return fechaProgramada;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void actualizarEstado(String estado) {
+        this.estado = estado;
+        System.out.println("La tarea '" + descripcion + "' se actualizó a: " + estado);
+    }
+}
+
