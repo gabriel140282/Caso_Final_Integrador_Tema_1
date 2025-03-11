28b08ec37246899d5bcd648c299008220e62f9a6
gabriel140282 <gkaakmor@myuax.com>
2025-03-11 12:08:19 +0100

    V1.4

--- src/main/java/org/example/VirtualGuide.java
+++ src/main/java/org/example/VirtualGuide.java
@@ -2,15 +2,6 @@ package org.example;
 
 public class VirtualGuide {
     public void proveerTour(Visitor visitor) {
-        System.out.println("\nBienvenido " + visitor.getNombre() + ". Iniciando tour personalizado basado en tus intereses: " + String.join(", ", visitor.getInterests()));
-        if(visitor.getIntereses().contains("mamíferos")) {
-            System.out.println("Te recomendamos visitar la sección de mamíferos.");
-        }
-        if(visitor.getIntereses().contains("aves")) {
-            System.out.println("Te recomendamos visitar la sección de aves.");
-        }
-        if(visitor.getIntereses().contains("reptiles")) {
-            System.out.println("Te recomendamos visitar la sección de reptiles.");
-        }
+        System.out.println("Proporcionando un tour virtual a " + visitor.getNombre());
     }
 }
